530518cbc2398d335c42c9513bcd7a656396750b
Sheeedsh78 <101755793+Sheeedsh78@users.noreply.github.com>
2024-05-13 04:39:25 +0200

    Update main.cpp

--- examples/example_win32_directx9/main.cpp
+++ examples/example_win32_directx9/main.cpp
@@ -13,6 +13,7 @@ static LPDIRECT3D9              g_pD3D = NULL;
 static LPDIRECT3DDEVICE9        g_pd3dDevice = NULL;
 static D3DPRESENT_PARAMETERS    g_d3dpp = {};
 
+
 // Forward declarations of helper functions
 bool CreateDeviceD3D(HWND hWnd);
 void CleanupDeviceD3D();
